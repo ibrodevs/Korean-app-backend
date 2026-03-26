2d58abb78d9236aa2ed03983a4c31a2edbb04ae1
gomanova <gomanova456@gmail.com>
2026-03-26 15:57:14 +0600

    реализован checkout и корзина

--- orders/models.py
+++ orders/models.py
@@ -60,7 +60,15 @@ class PickupLocation(models.Model):
     def __str__(self) -> str:
         return f"{self.city} — {self.name}"
 
+class ArrivalWindow(models.Model):
+    pickup_location = models.ForeignKey(PickupLocation, related_name='arrival_windows', on_delete=models.CASCADE)
+    date = models.DateField()
+    time_from = models.TimeField()
+    time_to = models.TimeField()
+    is_active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f"{self.pickup_location} {self.date} {self.time_from}-{self.time_to}"
 # ─────────────────────────────────────────────────────────────────────────────
 # Manager
 # ─────────────────────────────────────────────────────────────────────────────
@@ -595,3 +603,39 @@ class OrderStatusHistory(models.Model):
             comment=comment,
             changed_by=changed_by,
         )
+
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders_cart', )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart {self.id} ({self.user})"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product_variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    @property
+    def line_total(self):
+        return self.product_variant.price * self.quantity
+
+    def __str__(self):
+        return f"{self.product_variant} x {self.quantity}"
+
+class Coupon(models.Model):
+    PERCENT = 'percent'
+    FIXED = 'fixed'
+    DISCOUNT_TYPES = [
+        (PERCENT, 'percent'),
+        (FIXED, 'fixed')
+    ]
+
+    code = models.CharField(max_length=50, unique=True)
+    discount_type = models.CharField(max_length=10, choices=DISCOUNT_TYPES)
+    value = models.IntegerField()
+    min_order_amount = models.IntegerField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.code
